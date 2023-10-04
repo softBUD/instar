19ec798c4091104c86285de6b68cdbbccb1e04ac
softBUD <chevimpala21@gmail.com>
2023-10-04 17:41:42 +0900

    nav bar menu icon 추가, react-icon 패키지 설치

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+'use client';
+import Link from 'next/link';
+import React from 'react';
+import HomeOutlineIcon from './ui/icons/HomeOutlineIcon';
+import HomeFillIcon from './ui/icons/HomeFillIcon';
+import SearchIcon from './ui/icons/SearchIcon';
+import SearchFillIcon from './ui/icons/SearchFillIcon';
+import NewFillIcon from './ui/icons/NewFillIcon';
+import NewIcon from './ui/icons/NewIcon';
+import { usePathname } from 'next/navigation';
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const pathName = usePathname();
+  const menu = [
+    {
+      href: '/',
+      icon: <HomeOutlineIcon />,
+      clickedIcon: <HomeFillIcon />,
+    },
+    {
+      href: '/search',
+      icon: <SearchIcon />,
+      clickedIcon: <SearchFillIcon />,
+    },
+    {
+      href: '/new',
+      icon: <NewIcon />,
+      clickedIcon: <NewFillIcon />,
+    },
+  ];
+
+  return (
+    <div>
+      <Link href='/'>
+        <h1>Petsgram</h1>
+      </Link>
+      <nav>
+        <ul>
+          {menu.map((item) => (
+            <li key={item.href}>
+              <Link href={item.href}>
+                {pathName === item.href ? item.clickedIcon : item.icon}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
---
+++ components/ui/icons/HomeOutlineIcon.tsx
@@ -0,0 +1,7 @@
+import {AiOutlineHome} from 'react-icons/ai'
+
+export default function HomeOutlineIcon() {
+    return ( 
+        <AiOutlineHome className='w-7 h-7'/>
+    )
+}
